322b69cf1d6aa1e4bbdebaaeaa351c3724abefe5
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-12-26 18:38:19 -0300

    Aula11

--- aulas/aula11/script.js
+++ aulas/aula11/script.js
@@ -12,9 +12,10 @@ if (vel > 60) {
 function verificar() {
     let velocidade = Number(document.querySelector('#txtVel').value)
     console.log(velocidade)
+    document.querySelector('#vel').innerHTML = `Sua velocidade é de ${velocidade} km/h`
     if (velocidade > 60) {
-        document.querySelector('#res').innerHTML = '<p>Você ultrapassou a velocidade permitida. MULTADO!</p>'
+        document.querySelector('#res').innerHTML = 'Você ultrapassou a velocidade permitida. MULTADO!'
     } else {
-        document.querySelector('#res').innerHTML = '<p>Dirija sempre usando cinto de segurança</p>'
+        document.querySelector('#res').innerHTML = 'Dirija sempre usando cinto de segurança'
     }
 }
